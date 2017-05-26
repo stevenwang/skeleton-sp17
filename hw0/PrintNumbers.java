3b67b0987846515bdea2a737c02a18918bab0075
WX <stevenwang1985@gmail.com>
2017-05-25 17:16:55 -0700

    add hw1

---
+++ hw0/PrintNumbers.java
@@ -0,0 +1,10 @@
+public class PrintNumbers {
+public static void main(String[] args) {
+        int x ;
+        x = 0 ;
+        while(x < 10){
+          System.out.println(x) ;
+          x += 1;
+        }
+    }
+}
